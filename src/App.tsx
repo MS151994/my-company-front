23173f97ff6bd6c1fda96a00ac99636d21f89b56
Maciek <maciej.stepien1994@gmail.com>
2022-06-09 14:17:29 +0200

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {UserLogin} from "./components/UserForm/UserLogin";
+import {Header} from "./components/Header/Header";
+import {Route, Routes} from "react-router-dom";
 import {UserRegister} from "./components/UserForm/UserRegister";
 
 export const App = () => {
     return (
         <div>
-            <h1>my company manager </h1>
-            <UserLogin/>
+            <Header/>
+            <Routes>
+                <Route path="/login" element={<UserLogin/>}/>
+                <Route path="/register" element={<UserRegister/>}/>
+            </Routes>
         </div>
     )
 }
