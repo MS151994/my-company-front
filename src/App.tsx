d1782ace343b6fdb5b55cc0c2eff96b610e28a39
Maciek <maciej.stepien1994@gmail.com>
2022-06-09 19:47:13 +0200

    add config and cookie provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import {UserLogin} from "./components/UserForm/UserLogin";
 import {Header} from "./components/Header/Header";
 import {Route, Routes} from "react-router-dom";
 import {UserRegister} from "./components/UserForm/UserRegister";
+import {CookiesProvider} from "react-cookie";
+import {HomePage} from "./components/HomePage/HomePage";
 
 export const App = () => {
     return (
         <div>
-            <Header/>
-            <Routes>
-                <Route path="/login" element={<UserLogin/>}/>
-                <Route path="/register" element={<UserRegister/>}/>
-            </Routes>
+            <CookiesProvider>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/login" element={<UserLogin/>}/>
+                    <Route path="/register" element={<UserRegister/>}/>
+                </Routes>
+            </CookiesProvider>
         </div>
     )
 }
